a1d176e25f9a193a9564cccbd425ec610f71f5e8
NadineAbedrabba <abedrabbanadine123@gmail.com>
2025-04-22 11:08:25 +0100

    developpement du frontend auth

--- src/app/pages/authentication/side-login/side-login.component.ts
+++ src/app/pages/authentication/side-login/side-login.component.ts
@@ -1,29 +1,46 @@
+
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from 'src/app/material.module';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 
+
+
+
+
+
 @Component({
   selector: 'app-side-login',
-  imports: [RouterModule, MaterialModule, FormsModule, ReactiveFormsModule],
+  imports: [RouterModule,   CommonModule , MaterialModule, FormsModule, ReactiveFormsModule
+  ],
   templateUrl: './side-login.component.html',
 })
 export class AppSideLoginComponent {
-  constructor(private router: Router) {}
+  loginForm: FormGroup;
 
-  form = new FormGroup({
-    uname: new FormControl('', [Validators.required, Validators.minLength(6)]),
-    password: new FormControl('', [Validators.required]),
-  });
-
-  get f() {
-    return this.form.controls;
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
   }
 
-  submit() {
-    this.router.navigate(['']);
+  get email() {
+    return this.loginForm.get('email')!;
+  }
+
+  get password() {
+    return this.loginForm.get('password')!;
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('Form Submitted:', this.loginForm.value);
+      // envoyer vers API ou faire autre traitement ici
+    }
   }
 }
--- src/app/pages/authentication/side-register/side-register.component.ts
+++ src/app/pages/authentication/side-register/side-register.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 import { CoreService } from 'src/app/services/core.service';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
@@ -8,26 +10,38 @@ import { MaterialModule } from 'src/app/material.module';
 
 @Component({
   selector: 'app-side-register',
-  imports: [RouterModule, MaterialModule, FormsModule, ReactiveFormsModule],
+  imports: [RouterModule,   CommonModule , MaterialModule, FormsModule, ReactiveFormsModule ],
   templateUrl: './side-register.component.html',
 })
 export class AppSideRegisterComponent {
   options = this.settings.getOptions();
 
-  constructor(private settings: CoreService, private router: Router) {}
+  constructor(private settings: CoreService, private router: Router ,private fb: FormBuilder) {}
 
-  form = new FormGroup({
-    uname: new FormControl('', [Validators.required, Validators.minLength(6)]),
-    email: new FormControl('', [Validators.required]),
-    password: new FormControl('', [Validators.required]),
+  signupForm = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required]
   });
 
-  get f() {
-    return this.form.controls;
+  get name() {
+    return this.signupForm.get('name')!;
   }
 
-  submit() {
-    // console.log(this.form.value);
-    this.router.navigate(['/']);
+  get email() {
+    return this.signupForm.get('email')!;
   }
+
+  get password() {
+    return this.signupForm.get('password')!;
+  }
+
+  onSubmit(): void {
+    if (this.signupForm.valid) {
+      console.log('Signup data:', this.signupForm.value);
+      // Ici tu peux faire appel à un AuthService, enregistrer l'utilisateur, etc.
+    }
+  }
+
+  
 }
